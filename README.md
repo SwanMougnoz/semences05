40fe4bcbebc9665d2003726fb9fa504c9e742380
swan <mougnoz.swan@gmail.com>
2016-09-29 14:00:32 +0200

    Configuration de la base de donnée Mysql en dev + doc associée

---
+++ README.md
@@ -0,0 +1,9 @@
+# Semences05
+
+## Installation
+
+Installer les paquets :
+`sudo aptitude install python-dev python-mysqldb libmysqlclient-dev`
+
+Configurer django pour utiliser Mysql :
+`pip install MySQL-python`
--- semences05/settings/dev.py
+++ semences05/settings/dev.py
@@ -1,3 +1,14 @@
 from semences05.settings.common import *
 
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'semences05',
+        'USER': 'semences05',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
