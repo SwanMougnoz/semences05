d36bb5e0dceb917f7c4429eb5bc8ec5cf457e691
swan <mougnoz.swan@gmail.com>
2016-11-13 16:38:52 +0100

    Création d'un modèle pour les pages de contenu

--- s5vitrine/models/__init__.py
+++ s5vitrine/models/__init__.py
@@ -1,1 +1,2 @@
 from menuitem import Menuitem
+from page_contenu import PageContenu
---
+++ s5vitrine/models/page_contenu.py
@@ -0,0 +1,13 @@
+from django.db import models
+from s5vitrine.models import Menuitem
+
+
+class PageContenu(models.Model):
+    titre_page = models.CharField(max_length=255)
+    titre = models.CharField(max_length=255)
+    contenu = models.TextField()
+    lien_menu = models.OneToOneField(
+        Menuitem,
+        on_delete=models.CASCADE,
+        null=True
+    )
