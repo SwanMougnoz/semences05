0ecc0dc738c6465e0ff95af91b4028d0574ca5c4
swan <mougnoz.swan@gmail.com>
2016-11-14 13:39:29 +0100

    Génération des liens dans la template des menuitems

--- s5vitrine/models/lien.py
+++ s5vitrine/models/lien.py
@@ -1,9 +1,18 @@
+from django.core.urlresolvers import reverse, NoReverseMatch
 from django.db import models
 
 
 class Lien(models.Model):
     viewname = models.CharField(max_length=255)
-    params = models.CharField(max_length=255)
+    params = models.CharField(max_length=255, null=True)
+
+    @property
+    def url(self):
+        try:
+            url = reverse(self.viewname, args=self.params.split(' '))
+        except NoReverseMatch:
+            url = None
+        return url
 
     class Meta:
         unique_together = ('viewname', 'params')
