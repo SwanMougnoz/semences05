4d53a5f5d6b7592866a62c7b017aaf348f7adba3
swan <mougnoz.swan@gmail.com>
2016-11-27 14:53:12 +0100

    Génération de noms unique pour les fichiers de photo

--- s5appadherant/models/variete.py
+++ s5appadherant/models/variete.py
@@ -1,7 +1,17 @@
 from django.db import models
+import uuid
+import os
+
+
+def get_variete_upload_path(instance, filename):
+    # todo: trouver une solution pour ne pas garder les ancien fichier quand on change la photo
+    original_filename, extension = os.path.splitext(filename)
+    unique_filename = str(uuid.uuid4().hex)
+    filepath = 'varietes/{0}{1}'.format(unique_filename, extension)
+    return filepath
 
 
 class Variete(models.Model):
     nom = models.CharField(max_length=255)
     description = models.TextField(null=True)
-    photo = models.ImageField(upload_to='varietes', null=True)
+    photo = models.ImageField(upload_to=get_variete_upload_path, null=True)
