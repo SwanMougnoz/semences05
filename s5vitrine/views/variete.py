0a813b3972eead1d219af1a444e5621a44a72837
swan <mougnoz.swan@gmail.com>
2016-11-27 15:56:40 +0100

    Pagination des résultats

--- s5vitrine/views/variete.py
+++ s5vitrine/views/variete.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from s5appadherant.models import Variete
 from s5vitrine.models import Menuitem
 
@@ -10,8 +11,17 @@ class VarieteListView(TemplateView):
 
     def get(self, request, *args, **kwargs):
 
-        varietes = Variete.objects.all()
         menuitem = Menuitem.objects.get(pk='variete_list')
+
+        varietes_list = Variete.objects.all()
+        paginator = Paginator(varietes_list, 12)
+        page = request.GET.get('page')
+        try:
+            varietes = paginator.page(page)
+        except PageNotAnInteger:
+            varietes = paginator.page(1)
+        except EmptyPage:
+            varietes = paginator.page(paginator.num_pages)
 
         return self.render_to_response({
             'varietes': varietes,
