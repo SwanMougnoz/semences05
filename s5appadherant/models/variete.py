0873500c219262465281a2217fe7779a6a744560
swan <mougnoz.swan@gmail.com>
2016-11-27 14:37:07 +0100

    Remplacement de la propriete photo dans le modèle Variete par un champ ImageField + setup des urls media

--- s5appadherant/models/variete.py
+++ s5appadherant/models/variete.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Variete(models.Model):
-    nom = models.CharField(max_length=255, null=False)
-    description = models.TextField()
-    photo = models.URLField()
+    nom = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    photo = models.ImageField(upload_to='varietes', null=True)
--- semences05/admin.py
+++ semences05/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from s5vitrine.models import page, menuitem
+from s5appadherant.models import variete
 
 admin.site.register(page.PageContenu)
 admin.site.register(page.PageGenerique)
 admin.site.register(menuitem.Menuitem)
+admin.site.register(variete.Variete)
