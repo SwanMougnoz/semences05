2be8f93782987cfd3a9f08ee4be59d7bfb828370
swan <mougnoz.swan@gmail.com>
2016-12-06 21:32:29 +0100

    Integration du widget ckeditor dans l'admin pour une page de contenu

--- semences05/admin.py
+++ semences05/admin.py
@@ -1,8 +1,23 @@
+from django import forms
 from django.contrib import admin
+from ckeditor.widgets import CKEditorWidget
 from s5vitrine.models import page, menuitem
 from s5appadherant.models import variete
 
-admin.site.register(page.PageContenu)
+
+class PageContenuAdminForm(forms.ModelForm):
+    contenu = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = page.PageContenu
+        fields = ('titre_page', 'titre', 'contenu')
+
+
+class PageContenuAdmin(admin.ModelAdmin):
+    form = PageContenuAdminForm
+
+
+admin.site.register(page.PageContenu, PageContenuAdmin)
 admin.site.register(page.PageGenerique)
 admin.site.register(menuitem.Menuitem)
 admin.site.register(variete.Variete)
