674ecde7d253ce33a77b40a53b9e38cc01e6dce3
swan <mougnoz.swan@gmail.com>
2016-12-10 10:50:48 +0100

    Creation d'une vue pour la liste des varietes

--- s5appadherant/urls.py
+++ s5appadherant/urls.py
@@ -5,5 +5,6 @@ app_name = 's5appadherant'
 urlpatterns = [
     url(r'^$', views.AccueilView.as_view(), name='accueil_view'),
     url(r'^login/$', views.LoginView.as_view(), name='login_view'),
-    url(r'^logout/$', views.LogoutView.as_view(), name='logout_view')
+    url(r'^logout/$', views.LogoutView.as_view(), name='logout_view'),
+    url(r'^varietes/$', views.VarietelistView.as_view(), name='variete_list_view')
 ]
---
+++ s5appadherant/views/variete.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseNotFound
+from django.views.generic import TemplateView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from s5appadherant.models import Variete
+
+
+class VarietelistView(TemplateView):
+
+    template_name = 's5appadherant/variete_list.html'
+
+    def get(self, request, *args, **kwargs):
+
+        varietes_list = Variete.objects.all()
+        paginator = Paginator(varietes_list, 12)
+        page = request.GET.get('page')
+        try:
+            varietes = paginator.page(page)
+        except PageNotAnInteger:
+            varietes = paginator.page(1)
+        except EmptyPage:
+            varietes = paginator.page(paginator.num_pages)
+
+        return self.render_to_response({
+            'varietes': varietes,
+            'titre_page': 'Toutes les variétés',
+            'menu_actif': 'variete'
+        })
