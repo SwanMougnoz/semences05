853f95d27ac52785c5de7cf1589d409d9f350903
swan <mougnoz.swan@gmail.com>
2016-12-10 09:07:48 +0100

    fix formulaire contact: champs send_copy required

--- s5vitrine/forms/contact.py
+++ s5vitrine/forms/contact.py
@@ -5,4 +5,4 @@ class ContactForm(forms.Form):
     email = forms.EmailField(required=True)
     objet = forms.CharField(required=True, max_length=255)
     message = forms.CharField(required=True, widget=forms.Textarea)
-    send_copy = forms.BooleanField(initial=True, label="M'envoyer une copie par email")
+    send_copy = forms.BooleanField(initial=True, label="M'envoyer une copie par email", required=False)
