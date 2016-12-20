02d1a70b6bc634ef987af1f1b8bc0536b3e84f40
swan <mougnoz.swan@gmail.com>
2016-12-20 21:02:04 +0100

    Ajout d'un model Culture

---
+++ s5appadherant/models/culture.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+from s5appadherant.models import Jardin, Variete
+
+
+class Culture(models.Model):
+    CONSERVATION_ENUM = (
+        ('dynamique', 'Dynamique'),
+        ('conservatoire', 'Conservatoire')
+    )
+
+    jardin = models.ForeignKey(Jardin, on_delete=models.CASCADE)
+    variete = models.ForeignKey(Variete, on_delete=models.CASCADE)
+    type_conservation = models.CharField(max_length=255, choices=CONSERVATION_ENUM)
+    date_debut = models.DateField(auto_now_add=True)
+    date_fin = models.DateField(null=True)
+
