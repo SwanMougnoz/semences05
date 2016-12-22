0e0131d22fcf5b80969cc3fc95b79ad4d337c7ec
swan <mougnoz.swan@gmail.com>
2016-12-22 22:36:30 +0100

    Ajout des champs pour la creation d'une nouvelle variete

--- s5appadherant/views/culture.py
+++ s5appadherant/views/culture.py
@@ -1,49 +1,56 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ObjectDoesNotExist
-from django.core.urlresolvers import reverse
-from django.http import Http404
-from django.http import HttpResponseRedirect
-from django.views.generic import CreateView
+from django.http import HttpResponseNotFound
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 
 from s5appadherant.forms.culture import CultureForm
 from s5appadherant.models import Culture, Jardin
 
 
-class HttpRedirectResponse(object):
-    pass
-
-
-class CultureAddView(LoginRequiredMixin, CreateView):
+class CultureAddView(LoginRequiredMixin, TemplateView):
     template_name = 's5appadherant/culture/add.html'
-    form_class = CultureForm
-    model = Culture
 
-    def get_jardin(self):
-        jardin_id = self.kwargs.get('jardin_id')
+    def get(self, request, *args, **kwargs):
+        jardin_id = kwargs.get('jardin_id')
         try:
             jardin = Jardin.objects.get(pk=jardin_id)
         except ObjectDoesNotExist:
-            raise Http404
-        return jardin
+            return HttpResponseNotFound("<h1>La page demandee n'existe pas</h1>")
 
-    def get_success_url(self):
-        return reverse('s5appadherant:jardin_detail', kwargs={
-            'jardin_id': self.kwargs.get('jardin_id')
-        })
+        form = kwargs.get('form', CultureForm())
 
-    def get_context_data(self, **kwargs):
-        context = super(CultureAddView, self).get_context_data(**kwargs)
-        context.update({
-            'jardin': self.get_jardin(),
+        return self.render_to_response({
+            'jardin': jardin,
+            'form': form,
             'menu_actif': 'jardin',
             'titre_page': u"Ajout d'une variété cultivée"
         })
-        return context
 
-    def form_valid(self, form):
-        culture = form.save(commit=False)
-        culture.jardin = self.get_jardin()
-        culture.save()
-        return HttpResponseRedirect(self.get_success_url())
+    def post(self, request, *args, **kwargs):
+        jardin_id = kwargs.get('jardin_id')
+        try:
+            jardin = Jardin.objects.get(pk=jardin_id)
+        except ObjectDoesNotExist:
+            return HttpResponseNotFound("<h1>La page demandee n'existe pas</h1>")
 
+        form = CultureForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            variete = form.cleaned_data['variete']
+            variete.save()
+
+            culture = Culture()
+            culture.jardin = jardin
+            culture.variete = variete
+            culture.save()
+
+            return redirect('s5appadherant:jardin_detail', jardin_id=jardin.id)
+
+        return self.render_to_response({
+            'jardin': jardin,
+            'form': form,
+            'menu_actif': 'jardin',
+            'titre_page': u"Ajout d'une variété cultivée"
+        })
