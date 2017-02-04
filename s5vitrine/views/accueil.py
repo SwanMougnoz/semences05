0aec1024916a97e4ed9fb6d5e8bd60109261f7bc
swan <mougnoz.swan@gmail.com>
2017-02-04 16:53:15 +0100

    Lien entre le texte accueil et la page qui sommes nous

--- s5vitrine/views/accueil.py
+++ s5vitrine/views/accueil.py
@@ -1,5 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import TemplateView
-from s5vitrine.models.menuitem import Menuitem
+
+from s5vitrine.models import Menuitem
 
 
 class AccueilView(TemplateView):
@@ -10,10 +12,16 @@ class AccueilView(TemplateView):
     template_name = 's5vitrine/accueil.html'
 
     def get(self, request, *args, **kwargs):
-
         menuitem = Menuitem.objects.get(pk='accueil')
+
+        try:
+            qui_sommes_nous = Menuitem.objects.get(identifiant='qui-sommes-nous')
+            qsm_url = qui_sommes_nous.page.url
+        except ObjectDoesNotExist:
+            qsm_url = '#'
 
         return self.render_to_response({
             'menu_actif': menuitem,
+            'qui_sommes_nous_url': qsm_url,
             'titre_page': 'Accueil - Reseau semences Hautes-Alpes'
         })
