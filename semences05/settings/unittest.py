8f6c62f44b935204d66fbd3d7bebea7a4f6acfb4
swan <mougnoz.swan@gmail.com>
2017-02-11 22:39:29 +0100

    Setup travis.ci (3)

---
+++ semences05/settings/unittest.py
@@ -0,0 +1,17 @@
+from semences05.settings.common import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '::1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
