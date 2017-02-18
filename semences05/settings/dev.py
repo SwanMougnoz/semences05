136562745f93345875fa8bbd02ea55b07bc6750f
swan <mougnoz.swan@gmail.com>
2017-02-18 14:11:57 +0100

    Setting up django debug toolbar only for dev environment

--- semences05/settings/dev.py
+++ semences05/settings/dev.py
@@ -15,3 +15,6 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(PROJECT_DIR, 'var/emails')
+
+# Django debug toolbar
+# INTERNAL_IPS = ['127.0.0.1']
