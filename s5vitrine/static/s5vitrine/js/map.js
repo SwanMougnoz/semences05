2c8caa1a269f0e93b189d8d0ec347a4a9dcd02ba
swan <mougnoz.swan@gmail.com>
2017-02-25 14:35:29 +0100

    Affichage des vrais markeurs sur la carte vitrine

--- s5vitrine/static/s5vitrine/js/map.js
+++ s5vitrine/static/s5vitrine/js/map.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
-    var map = L.map('vitrine-map').setView([44.651369, 6.184484], 9);
+    var map = L.map('vitrine-map');
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
 
-    // Marker d'exemple
-    L.marker([44.899288, 6.645442]).addTo(map);
-    L.marker([44.914185, 6.403659]).addTo(map);
-    L.marker([44.796908, 6.566301]).addTo(map);
-    L.marker([44.561624, 6.081014]).addTo(map);
+    var markers = [];
+
+    var addMarker = function(index, jardin) {
+        var coords = [
+            jardin.adresse.latitude,
+            jardin.adresse.longitude
+        ];
+        var marker = L.marker(coords).addTo(map);
+        markers.push(marker);
+    };
+
+    $.ajax({
+        method: 'GET',
+        url: Urls['s5api:map_jardin_list'](),
+        dataType: 'json',
+        success: function(data) {
+            $(data).each(addMarker);
+            var group = new L.featureGroup(markers);
+            map.fitBounds(group.getBounds());
+        }
+    });
 });
